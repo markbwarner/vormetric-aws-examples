9cf48d4e43c579d75b354946cbee3b5acba4ef6a
markbwarner <markbwarner@gmail.com>
2020-06-03 17:21:06 -0400

    changed url to include parameters
    vtsdemo?verifyServerCertificate=false&useSSL=true";

--- src/main/java/com/vormetric/rest/aws_crypto_examples/ConnectionObject.java
+++ src/main/java/com/vormetric/rest/aws_crypto_examples/ConnectionObject.java
@@ -8,7 +8,7 @@ public class ConnectionObject {
    private static Connection conn = null;
    
    public static Connection getConnection(){
-	    String url = "jdbc:mysql://yourrds-instance";
+	    String url = "jdbc:mysql://yourrds-instance/vtsdemo?verifyServerCertificate=false&useSSL=true";
 
 	   try{
 		   Class.forName("com.mysql.jdbc.Driver").newInstance();
